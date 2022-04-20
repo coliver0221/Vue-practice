eb5699c9c656457285cf458a51752f6d1666ca3c
coliver0221 <coliver0221.cs03@nctu.edu.tw>
2022-04-20 15:27:41 +0800

    feat(src/): basic practice of setup and ref

--- first_practice/src/App.vue
+++ first_practice/src/App.vue
@@ -1,21 +1,28 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <!-- original code -->
+  <!-- <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/> -->
+
+  <SetupAndRefPractice />
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+// import HelloWorld from './components/HelloWorld.vue';
+import SetupAndRefPractice from './components/SetupAndRefPractice.vue';
 
 export default defineComponent({
   name: 'App',
   components: {
-    HelloWorld
+    // original component
+    // HelloWorld
+    SetupAndRefPractice
   }
 });
 </script>
 
 <style>
+/* original style for HelloWorld.vue */
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
---
+++ first_practice/src/components/SetupAndRefPractice.vue
@@ -0,0 +1,30 @@
+<template>
+    <h2>setup 和 ref 的基本使用</h2>
+    <h3>{{ count }}</h3>
+    <button @click="updateCount">Update</button>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue'
+
+export default defineComponent({
+    setup() {
+        // variables
+        // define a responsive variable which reference to an object and init
+        const count = ref(0)
+
+        // method
+        function updateCount() {
+            console.log('update click!')
+            count.value = count.value + 1
+        }
+
+        return {
+            // parameters
+            count,
+            // method
+            updateCount
+        }
+    },
+})
+</script>

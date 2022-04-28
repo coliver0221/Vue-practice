569d4cad76b40dfdc77ed09de5a39f49bea5f083
coliver0221 <coliver0221.cs03@nctu.edu.tw>
2022-04-28 16:04:10 +0800

    feat: practice variable passing from parent to child

--- first_practice/src/App.vue
+++ first_practice/src/App.vue
@@ -4,19 +4,24 @@
   <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/> -->
 
   <BasicPractice />
+  <hr>
+  <hr>
+  <VariablePassing />
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
 // import HelloWorld from './components/HelloWorld.vue';
 import BasicPractice from './components/BasicPractice.vue';
+import VariablePassing from './components/VariablePassing/Parent.vue';
 
 export default defineComponent({
   name: 'App',
   components: {
     // original component
     // HelloWorld
-    BasicPractice
+    BasicPractice,
+    VariablePassing
   }
 });
 </script>
---
+++ first_practice/src/components/VariablePassing/Parent.vue
@@ -0,0 +1,32 @@
+<template>
+    <h1>Parent Component</h1>
+    <h3>msg:{{ msg }}</h3>
+    <button @click="msg+=' Update by Parent!'">Update msg in parent</button>
+    <hr>
+    <!-- passing msg into Child -->
+    <Child :msg="msg" @updateMsg="updateMsg" />
+</template>
+<script lang="ts">
+import { defineComponent, ref } from 'vue'
+// import Child
+import Child from './Child.vue'
+export default defineComponent({
+    name:'ParentComponent',
+    components: {
+        Child,
+    },
+    setup() {
+        // define a variable and pass into child
+        const msg = ref('This msg is defined in parent component.')
+        // define a function for child to update msg
+        const updateMsg = (txt: string) => {
+            msg.value += txt
+        }
+
+        return {
+            msg,
+            updateMsg,
+        }
+    },
+})
+</script>

19c0be29b0e86ecc148e40d0a70a23ccc7335d94
coliver0221 <coliver0221.cs03@nctu.edu.tw>
2022-05-20 17:19:17 +0800

    feat(echarts/): complete basic pie chart

---
+++ echarts/src/components/PieChart.vue
@@ -0,0 +1,111 @@
+<template>
+  <div id="myBarChart" ref="pieChartContainer"></div>
+</template>
+
+<script>
+import { onMounted, onUnmounted, ref } from "vue";
+import * as echarts from "echarts";
+
+const generateSeries = (dataset) => {
+  const data = dataset.map((d) => {
+    return {
+      name: d.type,
+      value: d.count,
+    };
+  });
+  const totle = dataset.map((d) => d.count).reduce((prev, next) => prev + next);
+
+  const series = [
+    {
+      type: "pie",
+      name: "郵件類別",
+      radius: ["50%", "70%"],
+      label: {
+        show: false,
+      },
+      markPoint: {
+        tooltip: { show: false },
+        label: {
+          show: true,
+          formatter: "{b}",
+          color: "black",
+          fontSize: 30,
+          lineHeight: 30,
+        },
+        data: [
+          {
+            name: totle,
+            value: "-",
+            symbol: "circle",
+            itemStyle: { color: "transparent" },
+            x: "50%",
+            y: "45%",
+            // color: "#293845",
+          },
+          {
+            name: "封寄內郵件",
+            value: "-",
+            symbol: "circle",
+            itemStyle: { color: "transparent" },
+            x: "50%",
+            y: "55%",
+            color: "#B3BCC4",
+          },
+        ],
+      },
+      data,
+    },
+  ];
+
+  return series;
+};
+
+export default {
+  setup(props) {
+    const pieChartContainer = ref(null);
+
+    const drawChart = (pieChart) => {
+      const series = generateSeries(props.dataset);
+      console.log(series);
+      const option = {
+        tooltip: {
+          trigger: "item",
+        },
+        legend: {
+          left: "center",
+        },
+        series: series,
+      };
+
+      option && pieChart.setOption(option);
+    };
+
+    onMounted(() => {
+      let pieChart = echarts.init(pieChartContainer.value);
+      drawChart(pieChart);
+
+      window.onresize = function () {
+        pieChart.resize();
+      };
+    });
+
+    onUnmounted(() => {
+      window.onresize = null;
+    });
+
+    return { pieChartContainer, drawChart };
+  },
+  props: {
+    dataset: {
+      type: Array,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style>
+#myBarChart {
+  height: 500px;
+}
+</style>
--- echarts/src/views/HomeView.vue
+++ echarts/src/views/HomeView.vue
@@ -1,5 +1,55 @@
 <template>
   <div class="home">
-    <h1>This is home page</h1>
+    <!-- <h1>This is home page</h1> -->
+    <PieChart :dataset="dataset" />
   </div>
 </template>
+
+<script>
+import PieChart from "../components/PieChart";
+
+export default {
+  setup() {
+    const dataset = [
+      {
+        type: "安全信",
+        count: 162976,
+      },
+      {
+        type: "釣魚信",
+        count: 55874,
+      },
+      {
+        type: "勒索病毒",
+        count: 39577,
+      },
+      {
+        type: "BEC",
+        count: 11638,
+      },
+      {
+        type: "垃圾信",
+        count: 6986,
+      },
+      {
+        type: "勒索信",
+        count: 1746,
+      },
+      {
+        type: "灰色信件",
+        count: 582,
+      },
+      {
+        type: "可疑信件",
+        count: 103,
+      },
+    ];
+    return { dataset };
+  },
+  components: {
+    PieChart,
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>

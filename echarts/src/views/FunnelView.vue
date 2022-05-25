43989f003b804719ee9481fbc3159e1373671046
coliver0221 <coliver0221.cs03@nctu.edu.tw>
2022-05-25 14:25:19 +0800

    feat(echarts/): create route for funnel chart

---
+++ echarts/src/views/FunnelView.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="funnel">
+    <h1>This is page for funnel chart</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  setup() {
+    return {};
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>

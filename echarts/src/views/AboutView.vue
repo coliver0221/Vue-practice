e849c5de8a07d1abc211efefb66d59b691e7c9d5
coliver0221 <coliver0221.cs03@nctu.edu.tw>
2022-05-25 16:38:09 +0800

    feat(echarts/): add feature to generate daily or yearly data for bar chart

--- echarts/src/views/AboutView.vue
+++ echarts/src/views/AboutView.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="about">
     <!-- <h1>This is about page</h1> -->
-    <BarChart :dataset="dataset" />
+    <BarChart :dataset="dataset" :timeRange="timeRange" />
   </div>
 </template>
 
@@ -10,29 +10,61 @@ import BarChart from "../components/BarChart";
 import dayjs from "dayjs";
 import mailType from "../components/categories";
 
+const generateDailyData = () => {
+  const dataset = [],
+    startDate = dayjs("2021-01-01T00:00:00"),
+    endDate = dayjs("2021-01-01T23:59:59");
+
+  let currDate = startDate;
+  while (!currDate.isAfter(endDate)) {
+    let dailyData = {};
+    mailType.forEach((value) => {
+      dailyData[value] = Math.floor(Math.random() * 100);
+    });
+    dataset.push({
+      ...dailyData,
+      date: currDate.toISOString(),
+    });
+
+    currDate = currDate.add(1, "hour");
+  }
+
+  return dataset;
+};
+
+const generateYearlyData = () => {
+  const dataset = [],
+    startDate = dayjs("2021-01-01"),
+    endDate = dayjs("2021-12-31");
+
+  let currDate = startDate;
+  while (!currDate.isAfter(endDate)) {
+    let dailyData = {};
+    mailType.forEach((value) => {
+      dailyData[value] = Math.floor(Math.random() * 100);
+    });
+    dataset.push({
+      ...dailyData,
+      date: currDate.toISOString(),
+    });
+
+    currDate = currDate.add(1, "days");
+  }
+
+  return dataset;
+};
+
 export default {
   setup() {
-    // create dataset
-    const dataset = [],
-      startDate = dayjs("2021-01-01"),
-      // endDate = dayjs("2021-01-10");
-      endDate = dayjs("2021-12-31");
+    // generate dataset
+    // daily
+    // const dataset = generateDailyData();
+    // const timeRange = "daliy";
+    // yearly
+    const dataset = generateYearlyData();
+    const timeRange = "yearly";
 
-    let currDate = startDate;
-    while (!currDate.isAfter(endDate)) {
-      let dailyData = {};
-      mailType.forEach((value) => {
-        dailyData[value] = Math.floor(Math.random() * 100);
-      });
-      dataset.push({
-        ...dailyData,
-        date: currDate.toISOString(),
-      });
-
-      currDate = currDate.add(1, "days");
-    }
-
-    return { dataset };
+    return { dataset, timeRange };
   },
   components: {
     BarChart,

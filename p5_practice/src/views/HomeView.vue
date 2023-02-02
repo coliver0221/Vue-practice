7e3e251e7f1f323fb902245eb9cb5e065fdcd5bf
coliver0221 <coliver0221.cs03@nctu.edu.tw>
2023-02-02 21:26:57 +0800

    feat(views/HomeView.vue): create p5 block

--- p5_practice/src/views/HomeView.vue
+++ p5_practice/src/views/HomeView.vue
@@ -1,18 +1,34 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <p>P5 Testing</p>
+    <div ref="p5Container"></div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import P5 from "p5";
+import { onMounted, ref } from "vue";
 
 export default {
   name: "HomeView",
-  components: {
-    HelloWorld,
+  setup() {
+    let p5Container = ref(null);
+    const script = function (p5) {
+      // NOTE: Set up is here
+      p5.setup = () => {
+        p5.createCanvas(500, 500);
+      };
+      // NOTE: Draw is here
+      p5.draw = () => {
+        p5.background(0);
+      };
+    };
+
+    onMounted(() => {
+      new P5(script, p5Container.value);
+    });
+
+    return { p5Container };
   },
 };
 </script>
